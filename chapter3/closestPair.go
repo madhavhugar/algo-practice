06ab4626dffb7d349f618885c91c468e8473fc33
madhavhugar <madhav.hugar@gmail.com>
2020-05-31 13:59:48 +0200

    ClosestPair: Implement merge sorting points by x & y coordinate

--- chapter3/closestPair.go
+++ chapter3/closestPair.go
@@ -1,18 +1,83 @@
 package main
 
 type Point struct {
-	x int
-	y int
+	x float64
+	y float64
 }
 
-func closestPairBaseCase(points []Point, len int) {
-	for i := 0; i < len; i++ {
-
+func mergeSortPoints(points []Point, mergeFunc func([]Point, []Point) []Point) []Point {
+	if len(points) == 1 {
+		return points
 	}
+	a, b := splitPointsList(points)
+	c := mergeSortPoints(a, mergeFunc)
+	d := mergeSortPoints(b, mergeFunc)
+
+	return mergeFunc(c, d)
 }
 
-func closestPairOfPoints(pX []Point, pY []Point, len int) {
-	if len <= 3 {
-		return closestPairBaseCase(pX, len)
-	}
+func splitPointsList(points []Point) ([]Point, []Point) {
+	halfLen := len(points) / 2
+	return points[:halfLen], points[halfLen:]
 }
+
+func mergeListOfPointsSortedByXCoordinate(a []Point, b []Point) []Point {
+	halfLen := len(a)
+	length := halfLen * 2
+	c := make([]Point, length)
+	j := 0
+	k := 0
+
+	for i := 0; i < length; i++ {
+		if j >= halfLen {
+			c[i] = b[k]
+			k++
+		} else if k >= halfLen {
+			c[i] = a[j]
+			j++
+		} else if a[j].x < b[k].x {
+			c[i] = a[j]
+			j++
+		} else {
+			c[i] = b[k]
+			k++
+		}
+	}
+	return c
+}
+
+func mergeListOfPointsSortedByYCoordinate(a []Point, b []Point) []Point {
+	halfLen := len(a)
+	length := halfLen * 2
+	c := make([]Point, length)
+	j, k := 0, 0
+
+	for i := 0; i < length; i++ {
+		if j >= halfLen {
+			c[i] = b[k]
+			k++
+		} else if b[k].y < a[j].y {
+			c[i] = b[k]
+			k++
+		} else if k >= halfLen {
+			c[i] = a[j]
+			j++
+		} else {
+			c[i] = a[j]
+			j++
+		}
+	}
+	return c
+}
+
+// func closestPairBaseCase(points []Point, len int) {
+// 	for i := 0; i < len; i++ {
+
+// 	}
+// }
+
+// func closestPairOfPoints(pX []Point, pY []Point, len int) {
+// 	if len <= 3 {
+// 		return closestPairBaseCase(pX, len)
+// 	}
+// }
--- chapter3/closestPair_test.go
+++ chapter3/closestPair_test.go
@@ -1,9 +1,54 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestClosestPair(t *testing.T) {
-	// Base case
-	points := []Point{{x: 2, y: 2}, {x: 3, y: 3}, {x: 4, y: 4}}
-	closestPairOfPoints(points, 3)
+	assert "github.com/stretchr/testify/assert"
+)
+
+// func TestClosestPair(t *testing.T) {
+// 	// Base case
+// 	points := []Point{{x: 2, y: 2}, {x: 3, y: 3}, {x: 4, y: 4}}
+// 	closestPairOfPoints(points, 3)
+// }
+
+// func TestMergeSortPoints(t *testing.T) {
+// 	points := []Point{{x: 2, y: 2}, {x: 1, y: 3}, {x: 6, y: 4}, {x: 4, y: 4}}
+// 	got := mergeSortPointsByXCoordinate(points)
+// 	wanted := []Point{{x: 1, y: 3}, {x: 2, y: 2}, {x: 4, y: 4}, {x: 6, y: 4}}
+// 	assert.Equal(t, wanted, got)
+// }
+
+func TestMergeSortPoints(t *testing.T) {
+	points := []Point{{x: 2, y: 2}, {x: 1, y: 3}, {x: 6, y: 4}, {x: 4, y: 4}}
+	gotSortByX := mergeSortPoints(points, mergeListOfPointsSortedByXCoordinate)
+	wantedSortByX := []Point{{x: 1, y: 3}, {x: 2, y: 2}, {x: 4, y: 4}, {x: 6, y: 4}}
+	assert.Equal(t, wantedSortByX, gotSortByX)
+
+	points = []Point{{x: 1, y: 2}, {x: 4, y: 3}, {x: 2, y: 1}, {x: 3, y: 4}}
+	gotSortByY := mergeSortPoints(points, mergeListOfPointsSortedByYCoordinate)
+	wantedSortByY := []Point{{x: 2, y: 1}, {x: 1, y: 2}, {x: 4, y: 3}, {x: 3, y: 4}}
+	assert.Equal(t, wantedSortByY, gotSortByY)
+}
+
+func TestSplitPointsList(t *testing.T) {
+	points := []Point{{x: 2, y: 2}, {x: 1, y: 3}, {x: 6, y: 4}, {x: 4, y: 4}}
+	gotA, gotB := splitPointsList(points)
+	wantedA, wantedB := []Point{{x: 2, y: 2}, {x: 1, y: 3}}, []Point{{x: 6, y: 4}, {x: 4, y: 4}}
+	assert.Equal(t, wantedA, gotA)
+	assert.Equal(t, wantedB, gotB)
+}
+
+func TestMergeListOfPointsSortedByXCoordinate(t *testing.T) {
+	inputA, inputB := []Point{{x: 1, y: 2}, {x: 4, y: 3}}, []Point{{x: 2, y: 4}, {x: 3, y: 4}}
+	got := mergeListOfPointsSortedByXCoordinate(inputA, inputB)
+	wanted := []Point{{x: 1, y: 2}, {x: 2, y: 4}, {x: 3, y: 4}, {x: 4, y: 3}}
+	assert.Equal(t, wanted, got)
+}
+
+func TestMergeListOfPointsSortedByYCoordinate(t *testing.T) {
+	inputA, inputB := []Point{{x: 1, y: 2}, {x: 4, y: 3}}, []Point{{x: 2, y: 1}, {x: 3, y: 4}}
+	got := mergeListOfPointsSortedByYCoordinate(inputA, inputB)
+	wanted := []Point{{x: 2, y: 1}, {x: 1, y: 2}, {x: 4, y: 3}, {x: 3, y: 4}}
+	assert.Equal(t, wanted, got)
 }
