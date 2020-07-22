dab9e4b82c415f33a181ecb52481f7437b3898e2
madhavhugar <madhav.hugar@gmail.com>
2020-07-22 14:50:01 +0200

    Remove unrequired assignment

--- chapter8/depthFirstSearchRecursive.go
+++ chapter8/depthFirstSearchRecursive.go
@@ -7,10 +7,8 @@ func depthFirstSearchRecursive(graph Graph, startingVertex *Vertex) {
 	startingVertex.Explored = true
 	for _, edge := range graph.Edges {
 		if edge.Head == startingVertex {
-			edge.Tail.Explored = true
 			depthFirstSearchRecursive(graph, edge.Tail)
 		} else if edge.Tail == startingVertex {
-			edge.Head.Explored = true
 			depthFirstSearchRecursive(graph, edge.Head)
 		}
 	}
